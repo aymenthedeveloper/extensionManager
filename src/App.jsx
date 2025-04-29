67b19a5849799d833cd272c4ae9f3af6c9ffee47
aymenthedeveloper <aymendev.contact@gmail.com>
2025-04-29 21:02:22 +0100

    finished dark mode styling

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default App;
 function Wrapper({children}){
   return (
     <div className="light-gradient dark:dark-gradient font-[Noto_Sans]">
-      <div className="max-w-[1440px] mx-auto px-4.25 pt-5.25 pb-20 min-h-screen ">
+      <div className="max-w-[1440px] _mx-auto px-4.25 pt-5.25 pb-20 min-h-screen ">
         {children}
       </div>
     </div>
@@ -40,16 +40,16 @@ function ExtensionCard({name, description, logo, isActive, setExtensionsList}){
     })
   }
   return (
-    <div className={` bg-white dark:bg-clr-neutral-700 shadow-lg rounded-3xl grid grid-cols-[auto_1fr] _outline px-4.5 py-4.75 gap-x-4 gap-y-6 desktop:px-4.75 desktop:pt-4.25 grid-rows-[1fr_auto]`}>
+    <div className={` bg-white dark:bg-clr-neutral-800 dark:ring dark:ring-clr-neutral-100/50  shadow-lg rounded-3xl grid grid-cols-[auto_1fr] _outline px-4.5 py-4.75 gap-x-4 gap-y-6 desktop:px-4.75 desktop:pt-4.25 grid-rows-[1fr_auto]`}>
       <div className="">
         <img src={logo} alt="extension icon" />
       </div>
       <div>
-        <h2 className="font-bold text-[1.3rem]/6 mb-2 text-clr-neutral-900 desktop:text-xl/6">{name}</h2>
-        <p className="text-[15px]/5.5 text-clr-neutral-600">{description}</p>
+        <h2 className="font-bold text-[1.3rem]/6 mb-2 text-clr-neutral-900 desktop:text-xl/6 dark:text-white">{name}</h2>
+        <p className="text-[15px]/5.5 text-clr-neutral-600 dark:text-clr-neutral-200">{description}</p>
       </div>
       <div className="col-span-2 flex justify-between items-center pt-0.25">
-        <button className="border border-black/30 pl-[calc(var(--pad-x)+1px)] pr-[calc(var(--pad-x)-1px)] h-9 rounded-2xl tracking-tight [--pad-x:--spacing(4)]">Remove</button>
+        <button className="border border-black/30 dark:border-clr-neutral-100/50 pl-[calc(var(--pad-x)+1px)] pr-[calc(var(--pad-x)-1px)] h-9 rounded-2xl tracking-tight [--pad-x:--spacing(4)] dark:text-white">Remove</button>
         <ToggleBtn isChecked={isChecked} handleClick={handleClick}></ToggleBtn>
       </div>
     </div>
@@ -58,7 +58,7 @@ function ExtensionCard({name, description, logo, isActive, setExtensionsList}){
 
 function ToggleBtn({isChecked, handleClick}){
   return(
-    <button className={`relative w-(--btn-w) h-(--btn-h) rounded-[calc(var(--btn-h)/2)] bg-clr-neutral-300 cursor-pointer duration-200 after:content-[""] after:absolute after:rounded-full after:top-(--inner-pad) after:left-(--inner-pad) after:size-(--circle-size) after:bg-white after:duration-200  [--btn-h:--spacing(5)] [--btn-w:--spacing(9.25)] [--inner-pad:--spacing(0.75)] [--circle-size:calc(var(--btn-h)-var(--inner-pad)*2)] ${isChecked && "after:left-[calc(var(--btn-w)-var(--circle-size)-var(--inner-pad))] bg-clr-red-700"}`} onClick={handleClick} role="switch" aria-label="Toggle button">
+    <button className={`relative w-(--btn-w) h-(--btn-h) rounded-[calc(var(--btn-h)/2)] bg-clr-neutral-300 dark:bg-clr-neutral-600 cursor-pointer duration-200 after:content-[""] after:absolute after:rounded-full after:top-(--inner-pad) after:left-(--inner-pad) after:size-(--circle-size) after:bg-white after:duration-200  [--btn-h:--spacing(5)] [--btn-w:--spacing(9.25)] [--inner-pad:--spacing(0.5)] [--circle-size:calc(var(--btn-h)-var(--inner-pad)*2)] ${isChecked && "after:left-[calc(var(--btn-w)-var(--circle-size)-var(--inner-pad))] bg-clr-red-700 dark:bg-clr-red-400"}`} onClick={handleClick} role="switch" aria-label="Toggle button">
     </button>
   )
 }
@@ -80,7 +80,7 @@ function FilterBtn({selected, children, setCurrentFilter}){
     setCurrentFilter(children.toLowerCase())
   }
   return (
-    <button className={`cursor-pointer shadow-lg px-4.75 h-11 rounded-3xl text-xl  ${selected? "bg-clr-red-700 dark:bg-clr-red-500 dark:text-black text-white": "bg-white text-clr-neutral-900 dark:bg-clr-neutral-700 dark:text-white ring ring-clr-neutral-300/40 "}`} onClick={handleClick}>
+    <button className={`cursor-pointer shadow-lg px-4.75 h-11 rounded-3xl text-xl  ${selected? "bg-clr-red-700 dark:bg-clr-red-400 dark:text-black text-white": "bg-white text-clr-neutral-900 dark:bg-clr-neutral-700 dark:text-white ring ring-clr-neutral-300/40 "}`} onClick={handleClick}>
       {children}
     </button>
   )
@@ -149,7 +149,7 @@ function Header(props){
   return (
     <header className="shadow-sm min-h-16 rounded-lg bg-white dark:bg-clr-neutral-800 flex items-center justify-between px-2.75 max-w-[1170px] mx-auto desktop:mt-5 desktop:min-h-18.5 desktop:rounded-3xl desktop:px-4">
       <a href="#" className="cursor-pointer">
-        <img src="/images/logo.svg" alt="" />
+        <img src="/images/logo.svg" alt="logo icon" className="dark:invert-100" />
       </a>
       <DarkModeBtn />
     </header>
